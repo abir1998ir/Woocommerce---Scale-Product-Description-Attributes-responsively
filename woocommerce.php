a0078adb92767b83590b3e5f304e8df60c17b49b
Abir Howlader <128389016+abir1998ir@users.noreply.github.com>
2024-06-29 17:24:06 +0600

    Add files via upload

---
+++ woocommerce.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<?php
+	add_filter( 'woocommerce_product_tabs', 'exetera_custom_product_tabs', 98, 1 );
+
+function exetera_custom_product_tabs( $tabs ) { 
+
+    // Custom description callback.
+    $tabs['description']['callback'] = function() {
+    
+        global $post, $product;
+    
+        //Start of flex container
+        echo '<div class="desc-container">';
+
+        echo '<div class="description">';
+        the_content();
+        echo '</div>';
+
+        echo '<div class="specifications">';
+        echo '<h2>Specifications</h2>';
+        do_action( 'woocommerce_product_additional_information', $product );
+        echo '</div>';
+
+        //End of flex container
+        echo '</div>';
+    };
+
+    // Remove the additional information tab.
+    unset( $tabs['additional_information'] );
+
+    return $tabs;
+}
+	
+	
+	
+	?>
+</body>
+</html>
